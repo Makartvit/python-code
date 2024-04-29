f2d3741881ca6fc8077f0f9f8c8fd1488fb3d3ed
ARTEM MAKOGON <artemmak1991@gmail.com>
2024-04-29 17:10:54 +0300

    Homework_git

--- Makogon_6.py
+++ Makogon_6.py
@@ -40,17 +40,16 @@ class Pawn(ChessFigure):
             # Черные пешк движутся вверх.
             return row_diff == 1 and col_diff == 0
 
-    def move_pawn(self, new_place: tuple):
-        """Перемещает пешку в новое место"""
+    def move_pawn(self, new_place):
+        """Перемещает, если допустимо"""
         if self.can_move(new_place):
             self.moving_chess(new_place)
         else:
-            print("Ошибка")
+            raise ValueError("Недопустимое перемещение.")
 
 
 class Bishop(ChessFigure):
     def can_move(self, new_place):
-
         if not self.on_board(new_place):
             return False
 
@@ -60,19 +59,172 @@ class Bishop(ChessFigure):
         if row_diff == col_diff or row_diff == -col_diff:
             return True
 
+    def move(self, new_place):
+        """Перемещает, если допустимо"""
+        if self.can_move(new_place):
+            self.moving_chess(new_place)
+        else:
+            raise ValueError("Недопустимое перемещение.")
+
+
+class Rook(ChessFigure):
+
+    def can_move(self, new_place):
+        if not self.on_board(new_place):
+            return False
+
+        row_diff = new_place[0] - self.place_figure[0]
+        col_diff = new_place[1] - self.place_figure[1]
+
+        return row_diff == 0 or col_diff == 0
+
+    def move(self, new_place):
+        """Перемещает, если допустимо"""
+        if self.can_move(new_place):
+            self.moving_chess(new_place)
+        else:
+            raise ValueError("Недопустимое перемещение.")
+
+
+class Queen(ChessFigure):
+
+    def can_move(self, new_place):
+        if not self.on_board(new_place):
+            return False
+
+        row_diff = new_place[0] - self.place_figure[0]
+        col_diff = new_place[1] - self.place_figure[1]
+
+        Terms_of_the_Queens_transfer = row_diff == col_diff or row_diff == -col_diff or row_diff == 0 or col_diff == 0
+
+        if Terms_of_the_Queens_transfer:
+            return True
+
+    def move(self, new_place):
+        """Перемещает, если допустимо"""
+        if self.can_move(new_place):
+            self.moving_chess(new_place)
+        else:
+            raise ValueError("Недопустимое перемещение.")
+
+
+class King(ChessFigure):
+
+    def can_move(self, new_place):
+        if not self.on_board(new_place):
+            return False
+
+        row_diff = new_place[0] - self.place_figure[0]
+        col_diff = new_place[1] - self.place_figure[1]
+
+        Terms_of_the_King_transfer = (-1 <= row_diff <= 1) and (-1 <= col_diff <= 1)
+
+        if Terms_of_the_King_transfer:
+            return True
+
+    def move(self, new_place):
+        """Перемещает, если допустимо"""
+        if self.can_move(new_place):
+            self.moving_chess(new_place)
+        else:
+            raise ValueError("Недопустимое перемещение.")
+
+
+class Knight(ChessFigure):
+    def can_move(self, new_place):
+        if not self.on_board(new_place):
+            return False
+
+        row_diff = new_place[0] - self.place_figure[0]
+        col_diff = new_place[1] - self.place_figure[1]
+
+        # Конь ходит буквой "Г"
+        # Проверяем комбинацию шагов
+        Terms_of_the_Knight_transfer = (row_diff == 2 and col_diff == 1) or (row_diff == -2 and col_diff == -1) or \
+                                       (row_diff == -2 and col_diff == 1) or (row_diff == 2 and col_diff == -1) or \
+                                       (row_diff == 1 and col_diff == 2) or (row_diff == -1 and col_diff == -2) or \
+                                       (row_diff == 1 and col_diff == -2) or (row_diff == -1 and col_diff == 2)
+
+        return Terms_of_the_Knight_transfer
+
+    def move(self, new_place):
+        """Перемещает, если допустимо"""
+        if self.can_move(new_place):
+            self.moving_chess(new_place)
+        else:
+            raise ValueError("Недопустимое перемещение.")
+
+
+class ChessSet:
+    def __init__(self):
+        # Инициализируем фигур
+        self.figures = []
+
+    def add_figure(self, figure):
+        # Добавляет фигуру в список
+        self.figures.append(figure)
+
+    def get_figures(self):
+        # Возвращает список фигур
+        return self.figures
+
+    def find_movable_figures(self, target_position):
+        # Возвращает список фигур, которые могут переместиться
+        movable_figures = []
+        for figure in self.figures:
+            if figure.can_move(target_position):
+                movable_figures.append(figure)
+        return movable_figures
+
 
 pawn = Pawn("white", (6, 0))
 
-# Попытка переместить пешку на новое место
-pawn.move_pawn((5, 0))  # Ожидается перемещение
+pawn.move_pawn((5, 0))
 
-# Печать текущей позиции после перемещения
 print("Текущая позиция пешки:", pawn.place_figure)
 
 bishop = Bishop("white", (5, 5))
 
-bishop.moving_chess = (7, 7)
+bishop.moving_chess((3, 3))
 
 bishop.change_color()
 
 print(f"Текущая позиция слона: {bishop.place_figure}, Цвет слона: {bishop.color_figure}")
+
+king = King("white", (2, 7))
+
+king.move((1, 6))
+
+print(f"Текущая позиция king: {king.place_figure}, Цвет king: {king.color_figure}")
+
+knight = Knight("white", (2, 2))
+
+knight.move((4, 3))
+
+print(f"Текущая позиция knight: {knight.place_figure}, Цвет knight: {knight.color_figure}")
+
+chess_set = ChessSet()
+
+# Добавляем фигуры
+king = King("white", (0, 3))
+rook = Rook("white", (0, 0))
+bishop = Bishop("white", (0, 2))
+queen = Queen("white", (0, 2))
+knight = Knight("white", (0, 1))
+pawn = Pawn("white", (1, 1))
+
+chess_set.add_figure(king)
+chess_set.add_figure(rook)
+chess_set.add_figure(bishop)
+chess_set.add_figure(queen)
+chess_set.add_figure(knight)
+chess_set.add_figure(pawn)
+
+target_position = (1, 1)
+
+# Получаем список фигур, которые могут переместиться в указанное место
+movable_figures = chess_set.find_movable_figures(target_position)
+
+print("Фигуры, которые могут попасть в", target_position, ":")
+for figure in movable_figures:
+    print(figure.__class__.__name__)
